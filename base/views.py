4b47bf0dd9bcb3a42a9cc101c6c047a6ef9e4b6c
wgoma101 <wgoma101@gmail.com>
2024-09-08 16:07:06 -0400

    user login

--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
+from django.contrib.auth.models import User
 from .forms import RoomForm
 
 
@@ -11,9 +13,29 @@ from .forms import RoomForm
 #     {'id':3, 'name': 'Frontend developers'}
 # ]
 
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(email=email)
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q)  |
+        Q(description__icontains=q)  
+        )
+    topics = Topic.objects.all()
+
+    room_count = rooms.count()
+
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):

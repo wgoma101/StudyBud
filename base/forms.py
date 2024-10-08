06f0b2e6c81339d39820077e3e3a2bae7399956a
wgoma101 <wgoma101@gmail.com>
2024-10-07 21:34:17 -0400

    Theme installation

--- base/forms.py
+++ base/forms.py
@@ -4,4 +4,4 @@ from .models import Room
 class RoomForm(ModelForm):
     class Meta:
         model = Room
-        fields = '__all__'
+        exclude = ['host', 'participants'] # exclude these fields from the form
